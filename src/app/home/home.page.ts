0599114b06aa7b426c8895a9af0061a56cbd9471
Gwenael <gwenael.klock.sin@gmail.com>
2022-06-23 13:27:58 +0200

    first

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  tabfilms : any = [];
+  titre: any = '';
+  constructor(private http: HttpClient) {}
 
-  constructor() {}
+
+  calc(){
+    if(this.titre.length == 6){
+      this.titre = 'zeubi'
+    }
+
+  }
+
+  ionViewWillEnter() {
+
+    this.http.get('https://api.themoviedb.org/3/movie/popular?api_key=019047668672587abc30843ac633a54c')
+      .pipe(
+        map(response => response['results'])
+      )
+      .subscribe(films => {
+        this.tabfilms = films
+        this.titre = this.tabfilms[0].original_title;
+
+        console.log(this.tabfilms[0].original_title);
+      });
+  }
 
 }
